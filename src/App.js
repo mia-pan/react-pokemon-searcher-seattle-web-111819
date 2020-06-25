40e4095550cd5f28de51a9d9a443c0221d3fea87
Miapan <miapanyuewei@gmail.com>
2020-06-24 17:43:08 -0700

    poke

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+// import React from 'react'
+// import PokemonPage from './components/PokemonPage'
+// import PokemonIndex from './components/PokemonIndex'
+// import './App.css'
+// import './App.css'
+
+// const App = () => (
+//   <div className="App">
+//     <PokemonIndex />
+//   </div>
+// )
+
+// export default App
+
+
 import React from 'react'
-import PokemonPage from './components/PokemonPage'
+import PokemonIndex from './components/PokemonIndex'
 import './App.css'
 
 const App = () => (
   <div className="App">
-    <PokemonPage />
+    <PokemonIndex />
   </div>
 )
 
